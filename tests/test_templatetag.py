b13c78821efb0546d3ada7d942bdaa428b4becb0
Jake Scaltreto <jscaltreto@gmail.com>
2018-03-30 15:57:09 -0400

    Additional Test

--- tests/test_templatetag.py
+++ tests/test_templatetag.py
@@ -1,4 +1,7 @@
 import os
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.db.models.fields.files import ImageFieldFile, FileField
 from django.template import Template, Context
 from django.test import TestCase
 
@@ -7,13 +10,23 @@ try:
 except ImportError:
     import mock
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class TestTemplateTag(TestCase):
     TEMPLATE = Template('{% load simplethumb_tags %}{% simplethumb image spec %}')
 
     @mock.patch('simplethumb.models.os.stat')
-    def test_template_tag(self, mock_mtime):
+    def test_template_tag_static(self, mock_mtime):
         mock_mtime.return_value.st_mtime = '1234567890'
         rendered = self.TEMPLATE.render(Context({'image': 'cat.png', 'spec': ''}))
+
+        self.assertEqual(rendered, '/cat.png.EcGFxfc.png')
+
+    @mock.patch('simplethumb.models.os.stat')
+    def test_template_tag_imagefield(self, mock_mtime):
+        mock_mtime.return_value.st_mtime = '1234567890'
+        image = ImageFieldFile(instance=None, field=FileField(), name='cat.png')
+        rendered = self.TEMPLATE.render(Context({'image': image, 'spec': ''}))
 
         self.assertEqual(rendered, '/cat.png.EcGFxfc.png')
